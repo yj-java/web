5d9041224820947e7eecd1551c9b01022a82aaf2
yj <1229941055@qq.com>
2024-05-06 19:56:16 +0800

    redis基础api测试

--- src/main/java/org/yj/web/seckill/controller/RedisController.java
+++ src/main/java/org/yj/web/seckill/controller/RedisController.java
@@ -2,12 +2,15 @@ package org.yj.web.seckill.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yj.web.common.Result;
 import org.yj.web.common.StringRedisUtil;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @RequestMapping("/redis")
 @RestController
@@ -17,36 +20,126 @@ public class RedisController {
     private RedisTemplate redisTemplate;
     @Autowired
     private StringRedisUtil stringRedisUtil;
-
-    /**
-     * 添加String类型 key,value
-     */
-    @RequestMapping("/addStringAndTimeout")
-    public Result addStringAndTimeout(String key, String value, long timeout) {
-        redisTemplate.opsForValue().set(key, value, timeout);
-        return Result.success();
-    }
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 添加String类型 key,value
      */
     @RequestMapping("/addString")
     public Result addString(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+        stringRedisTemplate.opsForValue().set(key, value);
         return Result.success();
     }
 
+    /**
+     * 添加带过期时间的String类型 key,value
+     */
+    @RequestMapping("/addStringAndTimeout")
+    public Result addStringAndTimeout(String key, String value, long timeout) {
+        stringRedisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
+        return Result.success();
+    }
+
+    /**
+     * 添加map类型
+     */
+    @RequestMapping("/addMap")
+    public Result addMap(String key, String hashKey, String value) {
+        stringRedisTemplate.opsForHash().put(key, hashKey, value);
+        return Result.success();
+    }
+
+    /**
+     * 删除map类型
+     */
+    @RequestMapping("/deleteMap")
+    public Result deleteMap(String key, String hashKey) {
+        stringRedisTemplate.opsForHash().delete(key, hashKey);
+        return Result.success();
+    }
+
+    /**
+     * 查询map类型
+     */
+    @RequestMapping("/selectMap")
+    public Result selectMap(String key, String hashKey) {
+        String value = (String) stringRedisTemplate.opsForHash().get(key, hashKey);
+        return Result.success(value);
+    }
+
+    /**
+     * 查询map类型
+     */
+    @RequestMapping("/selectAllMap")
+    public Result selectAllMap(String key) {
+        Set<String> hashKeys = (Set) stringRedisTemplate.opsForHash().keys(key);
+        return Result.success(hashKeys);
+    }
 
     /**
      * 添加list类型
      */
     @RequestMapping("/addList")
-    public Result addRedis(String key, int index, String value) {
-        ArrayList<Object> objects = new ArrayList<>();
-        objects.add(value);
-        redisTemplate.opsForList().set(key, index, value);
+    public Result addList(String key, String value) {
+        stringRedisTemplate.opsForList().rightPush(key, value);
         return Result.success();
     }
+
+    /**
+     * 删除list类型
+     */
+    @RequestMapping("/deleteList")
+    public Result deleteList(String key, String value) {
+        stringRedisTemplate.opsForList().remove(key, 1, value);
+        return Result.success();
+    }
+
+    /**
+     * 修改list类型
+     */
+    @RequestMapping("/updateList")
+    public Result updateList(String key, int index, String value) {
+        stringRedisTemplate.opsForList().set(key, index, value);
+        return Result.success();
+    }
+
+    /**
+     * 查询list类型
+     */
+    @RequestMapping("/selectList")
+    public Result selectList(String key, long start, long end) {
+        List<String> range = stringRedisTemplate.opsForList().range(key, start, end);
+        return Result.success(range);
+    }
+
+    /**
+     * 添加set类型
+     */
+    @RequestMapping("/addSet")
+    public Result addSet(String key, String value) {
+        stringRedisTemplate.opsForSet().add(key, value);
+        return Result.success();
+    }
+
+    /**
+     * 删除set类型
+     */
+    @RequestMapping("/deleteSet")
+    public Result updateSet(String key, String value) {
+        stringRedisTemplate.opsForSet().remove(key, value);
+        return Result.success();
+    }
+
+    /**
+     * 查询set类型
+     */
+    @RequestMapping("/selectSet")
+    public Result selectSet(String key) {
+        Set<String> range = stringRedisTemplate.opsForSet().members(key);
+        return Result.success(range);
+    }
+
 
     /*
      删除redis key
