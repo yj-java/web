2e0faf01c6f43cb22746845918a42c23a6ce8774
yj <1229941055@qq.com>
2024-05-12 10:00:59 +0800

    秒杀详解

---
+++ src/main/java/org/yj/web/seckill/controller/SecKillController.java
@@ -0,0 +1,10 @@
+package org.yj.web.seckill.controller;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secKill")
+public class SecKillController {
+}
